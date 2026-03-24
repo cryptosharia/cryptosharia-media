025ef07103a94a4ab7f6707668c9fcb1147c1644
naufalnab <naufalnabila27@gmail.com>
2026-03-24 12:15:11 +0700

    feat: Add a global layout with a navigation progress loader component that displays during page transitions.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,12 @@
 	import Header from "$lib/components/Header.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import BottomNav from "$lib/components/BottomNav.svelte";
+	import NavigationLoader from "$lib/components/NavigationLoader.svelte";
 
 	let { children } = $props();
 </script>
 
+<NavigationLoader />
 <Seo />
 <Header />
 
